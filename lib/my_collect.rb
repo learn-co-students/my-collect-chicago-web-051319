aca14baaac8d9a980ef8c7ccbee5cb0b29ba234c
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-15 01:20:55 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(collection)
+  i = 0
+  arr = []
+  while i < collection.length
+    arr << yield(collection[i])
+    
+    
+    #yield a = collection[i]
+    #arr.push(a)
+    i += 1
+  end
+  return arr
+end
