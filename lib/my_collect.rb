d2b58f2c0b1e727a62c63e7fc4a303b82450091e
Emily R <emilyroulund@gmail.com>
2019-04-24 03:40:22 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,28 @@
 def my_collect(collection)
   i = 0
+  split = []
   while i < collection.length 
-  yield (collection[i].upcase)
-  i = i + 1 
-
+    if yield (collection[i].upcase)
+      split << collection [i]
+   end 
+   i+=1 
+  end 
+  split
 end 
 
 
 
+# my_collect(collection) do |name|
+# name.split(" ").first
+
+#def my_select(collection)
+#  i = 0
+#  select = []
+#  while i < collection.length
+#    if yield(collection[i])
+#      select << collection[i]
+#    end
+#    i+=1
+#  end
+#  select
+#end
